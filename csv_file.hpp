ac84ef05274a59c658838fe9747a53d37018bc8f
szhur <sergey.zhur.contractor@quodfinancial.com>
2021-06-23 15:43:39 +0300

    Add requested include

--- csv_file.hpp
+++ csv_file.hpp
@@ -1,3 +1,5 @@
+#include <fstream>
+
 #include "csv_row.hpp"
 
 class CsvFile
@@ -7,25 +9,25 @@ public:
     {
         std::ifstream infile(_path);
         std::string line;
-        
+
         while (std::getline(infile, line))
             m_rows.push_back(CsvRow(line, _delimeter));
     }
-    
+
     size_t size() const noexcept
     {
         return m_rows.size();
     }
-    
+
     // TODO: Add some possibility to iterate rows instead of this, so we not display internal logic
     CsvRow const & getRowByIndex(int _index) const
     {
         if (_index >= m_rows.size())
             throw "The index is more than a row size!";
-            
+
         return m_rows[_index];
     }
-    
+
 private:
     std::vector<CsvRow> m_rows;
 };
